d7b33f8526c3d22e4904a2cdd18cfeecc7b1d171
@krikun1983 <akhmadeevst@gmail.com>
2021-03-05 22:16:11 +0400

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  for (let i = 0; i < str.length; i += 1) {
+
+    for (let k = 0; k < bracketsConfig.length; k += 1) {
+
+      if ((str[i] === bracketsConfig[k][0]) && (str[i] === bracketsConfig[k][1])) {
+
+        if (str[i] === arr[arr.length - 1]) {
+          arr.pop();
+        } else {
+          arr.push(str[i]);
+        }
+      }
+
+      if (str[i] === bracketsConfig[k][0]) {
+        arr.push(str[i]);
+      }
+
+      if (str[i] === bracketsConfig[k][1]) {
+        if (arr[arr.length - 1] === bracketsConfig[k][0]) {
+          arr.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  return arr.length === 0;
 }
